ae901f324f000c39639b1a1003b056f21c2abbe1
Santiago Salem <salem_santiago@hotmail.com>
2016-06-17 12:10:00 -0300

    like y dislike funcionando

--- client/app/scripts/controllers/movie-view.js
+++ client/app/scripts/controllers/movie-view.js
@@ -11,33 +11,71 @@ angular.module('clientApp')
 .controller('MovieViewCtrl', function ( $scope, $routeParams,$location ,Movie, Like, auth) {
 
   $scope.viewMovie = true;
+  var botonNoMeGusta = document.getElementById('botonNoMeGusta');
+  var botonMeGusta = document.getElementById('botonMeGusta');
+
+  auth.profilePromise.then(function(profile){
+    Like.getList({movie: $routeParams.id, user: profile.user_id, vote: 0}).then(function(like){
+      if(like.length == 1){
+        botonMeGusta.style.opacity = "0.5";
+      }
+    });
+    Like.getList({movie: $routeParams.id, user: profile.user_id, vote: 1}).then(function(like){
+      if(like.length == 1){
+        botonNoMeGusta.style.opacity = "0.5";
+      }
+    });
+  });
+
+
 
   //Obtengo la pelicula que tiene el id indicado
   $scope.movie = Movie.one($routeParams.id).get().$object;
 
 
     $scope.liked = function() {
+
+      var botonNoMeGusta = document.getElementById('botonNoMeGusta');
       Movie.one($routeParams.id).get().then(function(movie) {
         auth.profilePromise.then(function(profile){
 
-          Like.getList({movie: $routeParams.id, user: profile.user_id}).then(function(like) {
-            //Obtengo la tupla que con los datos del usuario y la pelicula
-            //Si existe, luego el usuario ya voto, por lo tanto si vuelve a presionar se quita el like
-            if(like.length != 0){
-              //Debo quitar el like
+          Like.getList({movie: $routeParams.id, user: profile.user_id, vote: 0}).then(function(like){
+            if(like.length == 0){
+              //No fue realizado el dislike, luego se puede realizar un like
+              Like.getList({movie: $routeParams.id, user: profile.user_id}).then(function(like) {
+                //Obtengo la tupla que con los datos del usuario y la pelicula
+                //Si existe, luego el usuario ya voto, por lo tanto si vuelve a presionar se quita el like
 
-            }
-            else{
-              $scope.like={};
-              $scope.movie = movie;
-              $scope.movie.meGusta+=1;
-              $scope.like.user=profile.user_id;
-              $scope.like.movie=$routeParams.id;
-              $scope.like.vote=1;
+                if(like.length == 1){
 
-              $scope.movie.save();
-              Like.post($scope.like).then(function(){
-                $location.path('/movie/' + $routeParams.id);
+                  //Debo quitar el like
+                  $scope.movie = movie;
+                  $scope.movie.meGusta-=1;
+                  $scope.movie.save();
+                  botonNoMeGusta.style.opacity = "1";
+
+
+                  like[0].remove().then(function(){
+                    $location.path('/movie/' + $routeParams.id);
+                  });
+                }
+                else{
+                  if(like.length == 0){
+                    $scope.like={};
+                    $scope.movie = movie;
+                    $scope.movie.meGusta+=1;
+                    $scope.like.user=profile.user_id;
+                    $scope.like.movie=$routeParams.id;
+                    $scope.like.vote=1;
+
+                    $scope.movie.save();
+                    botonNoMeGusta.style.opacity = "0.5";
+
+                    Like.post($scope.like).then(function(){
+                      $location.path('/movie/' + $routeParams.id);
+                    });
+                  }
+                }
               });
             }
           });
@@ -46,31 +84,53 @@ angular.module('clientApp')
     };
 
     $scope.disliked = function() {
+
+      var botonMeGusta = document.getElementById('botonMeGusta');
       Movie.one($routeParams.id).get().then(function(movie) {
         auth.profilePromise.then(function(profile){
 
-          Like.getList({movie: $routeParams.id, user: profile.user_id}).then(function(like) {
-            //Obtengo la tupla que con los datos del usuario y la pelicula
-            //Si existe, luego el usuario ya voto, por lo tanto si vuelve a presionar se quita el like
-            if(like.length != 0){
-              //Debo quitar el like
+          Like.getList({movie: $routeParams.id, user: profile.user_id, vote: 1}).then(function(like){
+            if(like.length == 0){
+              //No fue realizado el like, luego se puede realizar un dislike
+              Like.getList({movie: $routeParams.id, user: profile.user_id}).then(function(like) {
+                //Obtengo la tupla que con los datos del usuario y la pelicula
+                //Si existe, luego el usuario ya voto, por lo tanto si vuelve a presionar se quita el dislike
 
-            }
-            else{
-              $scope.like={};
-              $scope.movie = movie;
-              $scope.movie.noMeGusta+=1;
-              $scope.like.user=profile.user_id;
-              $scope.like.movie=$routeParams.id;
-              $scope.like.vote=0;
+                if(like.length == 1){
 
-              $scope.movie.save();
-              Like.post($scope.like).then(function(){
-                $location.path('/movie/' + $routeParams.id);
+                  //Debo quitar el dislike
+                  $scope.movie = movie;
+                  $scope.movie.noMeGusta-=1;
+                  $scope.movie.save();
+                  botonMeGusta.style.opacity = "1";
+
+
+                  like[0].remove().then(function(){
+                    $location.path('/movie/' + $routeParams.id);
+                  });
+                }
+                else{
+                  if(like.length == 0){
+                    $scope.like={};
+                    $scope.movie = movie;
+                    $scope.movie.noMeGusta+=1;
+                    $scope.like.user=profile.user_id;
+                    $scope.like.movie=$routeParams.id;
+                    $scope.like.vote=0;
+
+                    $scope.movie.save();
+                    botonMeGusta.style.opacity = "0.5";
+
+                    Like.post($scope.like).then(function(){
+                      $location.path('/movie/' + $routeParams.id);
+                    });
+                  }
+                }
               });
             }
           });
         });
       });
     };
-});
+
+  });
