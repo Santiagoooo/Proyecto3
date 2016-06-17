7c654bf2d20765eee45fe48daba3d7be16d97c4f
kevschweitzer <kevschweitzer@gmail.com>
2016-06-16 21:12:12 -0300

    dislike

--- client/app/scripts/controllers/movie-view.js
+++ client/app/scripts/controllers/movie-view.js
@@ -46,17 +46,31 @@ angular.module('clientApp')
     };
 
     $scope.disliked = function() {
-      Movie.one($routeParams.id).get().then(function(movie,like) {
-        $scope.movie = movie;
-        $scope.like = like;
-        $scope.movie.noMeGusta+=1;
-        $scope.like={"user":$routeParams.id,"vote":0};
-      // PUT /movie/id/ Save tiene la capacidad de saber si tiene que realizar el put o el post
-      $scope.movie.save().then(function() {
-        $location.path('/movie/' + $routeParams.id);
-        });
+      Movie.one($routeParams.id).get().then(function(movie) {
+        auth.profilePromise.then(function(profile){
 
+          Like.getList({movie: $routeParams.id, user: profile.user_id}).then(function(like) {
+            //Obtengo la tupla que con los datos del usuario y la pelicula
+            //Si existe, luego el usuario ya voto, por lo tanto si vuelve a presionar se quita el like
+            if(like.length != 0){
+              //Debo quitar el like
+
+            }
+            else{
+              $scope.like={};
+              $scope.movie = movie;
+              $scope.movie.noMeGusta+=1;
+              $scope.like.user=profile.user_id;
+              $scope.like.movie=$routeParams.id;
+              $scope.like.vote=0;
+
+              $scope.movie.save();
+              Like.post($scope.like).then(function(){
+                $location.path('/movie/' + $routeParams.id);
+              });
+            }
+          });
+        });
       });
     };
-
 });
